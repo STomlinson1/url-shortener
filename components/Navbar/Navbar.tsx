2106b9d7f63d69d80475c01615a6613bbc9cb199
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-30 13:20:46 -0400

    Added react-icons library. Added responsive styles for Nav and state for opening/closing mobile nav.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,34 +1,53 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import { RiMenu4Line, RiCloseLine } from 'react-icons/ri';
+
 import s from './Navbar.module.scss';
 
 import Logo from '../Logo/Logo';
 import Button from '../Button/Button';
 
 const Navbar: React.FC = () => {
+	const [ open, setOpen ] = useState(false);
+
 	return (
 		<nav className={`${s.navbar}`}>
 			<div className="container-l flex-row">
 				<Logo />
 				<ul className={s.group}>
 					<li>
-						<a>Features</a>
+						<Link href="/">
+							<a>Features</a>
+						</Link>
 					</li>
 					<li>
-						<a>Pricing</a>
+						<Link href="/">
+							<a>Pricing</a>
+						</Link>
 					</li>
 					<li>
-						<a>Resources</a>
+						<Link href="/">
+							<a>Resources</a>
+						</Link>
 					</li>
 				</ul>
 				<ul className={s.group}>
 					<li>
-						<a>Login</a>
+						<Link href="/">
+							<a>Login</a>
+						</Link>
 					</li>
 					<li>
-						<a className={s.btnContainer}>
-							<Button text="Sign up" />
-						</a>
+						<Link href="/">
+							<a className={s.btnContainer}>
+								<Button text="Sign up" />
+							</a>
+						</Link>
 					</li>
 				</ul>
+				<div className={s.navButtonContainer} onClick={() => setOpen(!open)}>
+					{open ? <RiCloseLine /> : <RiMenu4Line />}
+				</div>
 			</div>
 		</nav>
 	);
