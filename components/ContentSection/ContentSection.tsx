c7034e3f3aeb774ae2000d5c7514b008c563dff2
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-04-01 13:38:00 -0400

    Add card data to contentsection. Will eventually refactor this to its own file.

--- components/ContentSection/ContentSection.tsx
+++ components/ContentSection/ContentSection.tsx
@@ -1,6 +1,29 @@
 import s from './ContentSection.module.scss';
 
 import Button from '../Button/Button';
+import Card from '../Card/Card';
+import { Fragment } from 'react';
+
+const cardData = [
+	{
+		title: 'Brand Recognition',
+		content:
+			'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
+		imgUrl: '/images/icon-brand-recognition.svg'
+	},
+	{
+		title: 'Detailed Records',
+		content:
+			'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+		imgUrl: '/images/icon-detailed-records.svg'
+	},
+	{
+		title: 'Fully Customizable',
+		content:
+			'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+		imgUrl: '/images/icon-fully-customizable.svg'
+	}
+];
 
 const ContentSection: React.FC = () => {
 	return (
@@ -23,7 +46,20 @@ const ContentSection: React.FC = () => {
 					Track how your links are performing across the web with our advanced
 					statistics dashboard
 				</p>
-				<div className={s.cardContainer}>Cards</div>
+				<div className={s.cardContainer}>
+					{cardData.map((card, idx) => {
+						if (idx === cardData.length - 1) {
+							return <Card key={card.title} {...card} />;
+						}
+						return (
+							<Fragment key={card.title}>
+								<Card {...card} />
+								<div className={s.divider} />
+							</Fragment>
+						);
+					})}
+				</div>
+				<div className={s.block} />
 			</div>
 		</section>
 	);
