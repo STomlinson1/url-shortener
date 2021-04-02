0534f8236d64f7c21cde416522f30cec9164e9ed
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-04-02 16:09:25 -0400

    Add responsive styles to footer.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import s from './Footer.module.scss';
 
 import Logo from '../../public/images/logo-white.svg';
@@ -5,6 +7,68 @@ import Facebook from '../../public/images/icon-facebook.svg';
 import Twitter from '../../public/images/icon-twitter.svg';
 import Pinterest from '../../public/images/icon-pinterest.svg';
 import Instagram from '../../public/images/icon-instagram.svg';
+import { link } from 'node:fs';
+
+const linkGroups = [
+	[
+		{
+			text: 'Features',
+			href: '/'
+		},
+		{
+			text: 'Link Shortening',
+			href: '/'
+		},
+		{
+			text: 'Branded Links',
+			href: '/'
+		},
+		{
+			text: 'Analytics',
+			href: '/'
+		}
+	],
+	[
+		{
+			text: 'Resources',
+			href: '/'
+		},
+		{
+			text: 'Blog',
+			href: '/'
+		},
+		{
+			text: 'Developers',
+			href: '/'
+		},
+		{
+			text: 'Support',
+			href: '/'
+		}
+	],
+	[
+		{
+			text: 'Company',
+			href: '/'
+		},
+		{
+			text: 'About',
+			href: '/'
+		},
+		{
+			text: 'Our Team',
+			href: '/'
+		},
+		{
+			text: 'Careers',
+			href: '/'
+		},
+		{
+			text: 'Contact',
+			href: '/'
+		}
+	]
+];
 
 const Footer = () => {
 	return (
@@ -14,25 +78,17 @@ const Footer = () => {
 					<Logo />
 				</div>
 				<div className={s.linkContainer}>
-					<ul>
-						<li>Features</li>
-						<li>Link Shortening</li>
-						<li>Branded Links</li>
-						<li>Analytics</li>
-					</ul>
-					<ul>
-						<li>Resources</li>
-						<li>Blog</li>
-						<li>Developers</li>
-						<li>Support</li>
-					</ul>
-					<ul>
-						<li>Company</li>
-						<li>About</li>
-						<li>Our Team</li>
-						<li>Careers</li>
-						<li>Contact</li>
-					</ul>
+					{linkGroups.map((links, idx) => {
+						return (
+							<ul key={idx} className={s.group}>
+								{links.map((link) => (
+									<Link href={link.href} key={link.text}>
+										<a>{link.text}</a>
+									</Link>
+								))}
+							</ul>
+						);
+					})}
 				</div>
 				<div className={s.social}>
 					<Facebook />
@@ -41,6 +97,9 @@ const Footer = () => {
 					<Instagram />
 				</div>
 			</div>
+			<p className={`${s.signature} container-l`}>
+				Copyright &copy; 2021, Developed by Stephon Tomlinson
+			</p>
 		</footer>
 	);
 };
