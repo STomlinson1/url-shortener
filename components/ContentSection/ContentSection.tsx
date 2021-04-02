3fd5676e03026ec4645fdf8e94f1ec45301a93f2
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-04-02 16:20:12 -0400

    move card data to separate file.

--- components/ContentSection/ContentSection.tsx
+++ components/ContentSection/ContentSection.tsx
@@ -4,26 +4,7 @@ import Button from '../Button/Button';
 import Card from '../Card/Card';
 import { Fragment } from 'react';
 
-const cardData = [
-	{
-		title: 'Brand Recognition',
-		content:
-			'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
-		imgUrl: '/images/icon-brand-recognition.svg'
-	},
-	{
-		title: 'Detailed Records',
-		content:
-			'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
-		imgUrl: '/images/icon-detailed-records.svg'
-	},
-	{
-		title: 'Fully Customizable',
-		content:
-			'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
-		imgUrl: '/images/icon-fully-customizable.svg'
-	}
-];
+import { cardData } from './data';
 
 const ContentSection: React.FC = () => {
 	return (
---
+++ components/ContentSection/data.ts
@@ -0,0 +1,20 @@
+export const cardData = [
+	{
+		title: 'Brand Recognition',
+		content:
+			'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
+		imgUrl: '/images/icon-brand-recognition.svg'
+	},
+	{
+		title: 'Detailed Records',
+		content:
+			'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+		imgUrl: '/images/icon-detailed-records.svg'
+	},
+	{
+		title: 'Fully Customizable',
+		content:
+			'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+		imgUrl: '/images/icon-fully-customizable.svg'
+	}
+];
