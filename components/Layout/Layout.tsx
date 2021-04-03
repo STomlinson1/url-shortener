f2029e0cca996031821221381175322f1c20f086
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-04-02 20:00:47 -0400

    Bring footer into layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 
 import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 const Layout: React.FC = ({ children }) => {
 	return (
 		<Fragment>
 			<Navbar />
 			{children}
-			{/* Footer */}
+			<Footer />
 		</Fragment>
 	);
 };
