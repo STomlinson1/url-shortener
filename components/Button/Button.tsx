c421bb970a34643d1c342af0f490127aa2d4ba6b
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-04-07 11:47:21 -0400

    Added React-Spinner library. Added Hashloader to Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,20 +1,29 @@
 import s from './Button.module.scss';
 
+import HashLoader from 'react-spinners/HashLoader';
 interface ButtonProps {
 	text: string;
 	variant: 'primary' | 'secondary';
 	boxed?: boolean;
 	onClick?: () => void;
+	loading?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, variant, onClick, boxed }) => {
+const Button: React.FC<ButtonProps> = ({
+	text,
+	variant,
+	onClick,
+	boxed,
+	loading
+}) => {
 	if (onClick) {
 		return (
 			<button
 				onClick={onClick}
 				className={`${s.button} ${s[variant]} ${boxed ? s['boxed'] : ''}`}
+				disabled={loading}
 			>
-				{text}
+				{loading ? <HashLoader color="#fff" size={30} /> : <span>{text}</span>}
 			</button>
 		);
 	}
