521fac5ab3b96833330c4f4641a8db40568b1900
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-04-07 14:44:00 -0400

    Responsive styles for links

--- components/Links/Links.tsx
+++ components/Links/Links.tsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from 'react';
+
 import s from './Links.module.scss';
 
 import Button from '../Button/Button';
@@ -8,22 +10,30 @@ interface LinksProps {
 	links: ApiData[];
 }
 
+interface LinkProps {
+	link: ApiData;
+}
+
+const Link: React.FC<LinkProps> = ({ link }) => {
+	return (
+		<li className={s.link}>
+			<p className={s.original}>{link.result.original_link}</p>
+			<div className={s.group}>
+				<a target="_blank" href={link.result.full_short_link}>
+					<p>{link.result.full_short_link}</p>
+				</a>
+				<div className={s.btnContainer}>
+					<Button text="Copy" variant="primary" boxed />
+				</div>
+			</div>
+		</li>
+	);
+};
+
 const Links: React.FC<LinksProps> = ({ links }) => {
 	return (
 		<ul className={s.links}>
-			{links.map((link) => (
-				<li className={s.link} key={link.result.code}>
-					<p className={s.original}>{link.result.original_link}</p>
-					<div className={s.group}>
-						<a target="_blank" href={link.result.full_short_link}>
-							<p>{link.result.full_short_link}</p>
-						</a>
-						<div className={s.btnContainer}>
-							<Button text="Copy" variant="primary" boxed />
-						</div>
-					</div>
-				</li>
-			))}
+			{links.map((link) => <Link link={link} key={link.result.code} />)}
 		</ul>
 	);
 };
